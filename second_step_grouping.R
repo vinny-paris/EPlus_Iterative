24d2462ea2be1876eed59404c7b3d8091ee61dbb
Vinny <vinny@vinny.nomad.utk.edu>
2019-06-25 16:13:38 -0400

    none variable factors are not set to the defaults for the design matrix

--- second_step_grouping.R
+++ second_step_grouping.R
@@ -8,11 +8,16 @@ grouping_it <- function(data, sig_groups = NULL, num_of_groups = 10){
   if(length(sig_groups) == 0) {
     sig_groups <- 1:max(data$Group)
     }
-  new_groups <- subset(data, data$Group %in% sig_groups)
+  new_groupss <- subset(data, data$Group %in% sig_groups)
   
 
-  new_groups <- create_grouping_exp(data = new_groups, num_of_groups = num_of_groups)
-  names(new_groups) <- c("param_data", "design_matrix")
+ 
+  exp_data <- make_groups(group_size = num_of_groups, data = new_groupss) 
+  design_matrix <- design_with_defaults(data_summary = exp_data, data = data) 
   
-  return(new_groups)
+  new_groupss <- list(exp_data, design_matrix)
+  
+  names(new_groupss) <- c("param_data", "design_matrix")
+  
+  return(new_groupss)
 }
