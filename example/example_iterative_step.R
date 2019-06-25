058aaadc9277293461a16f27e9e7807d0e35c60b
Vinny <vinny@vinny.nomad.utk.edu>
2019-06-25 16:13:02 -0400

    corrected file path

--- example/example_iterative_step.R
+++ example/example_iterative_step.R
@@ -1,5 +1,5 @@
-data <- read.csv("data_update.csv", header = TRUE)
-design_matrix <- read.csv("design_matrix.csv", header = TRUE)[,-1]
+data <- read.csv("./data/data_update.csv", header = TRUE)
+design_matrix <- read.csv("./data/design_matrix.csv", header = TRUE)[,-1]
 
 
 l <- iterative_grouping(response, data, design_matrix)
