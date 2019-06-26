1184cc699d257ed22040e16fcdd2504e042cdefc
Vinny <vinny@vinny.nomad.utk.edu>
2019-06-26 15:35:21 -0400

    expanded example showing how the iterative process works under the hood

--- example/example_iterative_step.R
+++ example/example_iterative_step.R
@@ -1,8 +1,14 @@
 data <- read.csv("./data/data_update.csv", header = TRUE)
 design_matrix <- read.csv("./data/design_matrix.csv", header = TRUE)[,-1]
 
+response1 <- rnorm(1024)
+g <- iterative_grouping(response1, data, design_matrix)
 
-l <- iterative_grouping(response, data, design_matrix)
 
+response2 <- rnorm(1024)
+h <- iterative_grouping(response2, g[[1]], g[[2]])
+
+response3 <- rnorm(1024)
+i <- iterative_grouping(response3, h[[1]], h[[2]])
 
 
