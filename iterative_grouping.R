7004944f20a0da8348c0ca863e172fe6b612e26b
Vinny <vinny@vinny.nomad.utk.edu>
2019-06-26 15:33:14 -0400

    Added a catch for variables that are held constant through the design

--- iterative_grouping.R
+++ iterative_grouping.R
@@ -7,7 +7,8 @@
 
 
 iterative_grouping <- function(response, data, design_matrix, num_of_groups = 10, new_groups = 6){
-  r <- Lenth(response, design_matrix = design_matrix, data = data, new_groups = new_groups)
+  actual_design <- design_matrix[,which(apply(design_matrix, 2, fu) > 1)]
+  r <- Lenth(response, design_matrix = actual_design, data = data, new_groups = new_groups)
   new_data <- grouping_it(data, sig_groups = r, num_of_groups = num_of_groups)
   return(new_data)
 }
